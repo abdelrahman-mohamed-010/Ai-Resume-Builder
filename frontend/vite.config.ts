0963be6c8be6f04bd31aa6a92e1fd8c0cd8d833c
body <abdo.mhamed122yahoo.com@gmail.com>
2024-11-01 03:19:49 +0200

    vite configration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "esnext", // or 'es2015' if older compatibility is needed
+  },
+});
