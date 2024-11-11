e206dfb1e34f64a13bd347da9fc02216d725af3d
body <abdo.mhamed122yahoo.com@gmail.com>
2024-11-11 19:51:32 +0200

    feat: add rich text editor

--- frontend/src/pages/resume-builder/Custom.tsx
+++ frontend/src/pages/resume-builder/Custom.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { IoIosArrowForward } from "react-icons/io";
 import Stepper from "./Stepper";
 import ResumeOverview from "./ResumeOverview";
+import TxtEditor from "./TxtEditor";
 
 const Custom = () => {
   const navigate = useNavigate();
@@ -35,9 +36,13 @@ const Custom = () => {
               This is the perfect place to highlight any unique experience or
               skill.
             </p>
+            <div className=" mt-2 max-w-[calc(100wh-400px)]">
+              <TxtEditor />
+            </div>
           </div>
-          <div className=" flex justify-end flex-col items-end gap-7 mt-4">
+          <div className=" flex justify-end flex-col items-end gap-7 mt-4 ">
             <ResumeOverview />
+
             <Link
               to={"/builder/custom"}
               className=" bg-black px-5 py-3 text-lg rounded-full text-white font-semibold shadow-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg flex items-center gap-1 dark:bg-accent dark:text-black w-fit"
---
+++ frontend/src/pages/resume-builder/TxtEditor.tsx
@@ -0,0 +1,46 @@
+import { useState, ChangeEvent } from "react";
+import {
+  BtnBold,
+  BtnBulletList,
+  BtnItalic,
+  BtnLink,
+  BtnNumberedList,
+  BtnStrikeThrough,
+  BtnUnderline,
+  Editor,
+  EditorProvider,
+  Toolbar,
+  Separator,
+} from "react-simple-wysiwyg";
+
+export default function TxtEditor() {
+  const [value, setValue] = useState<string>();
+
+  function onChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    setValue(e.target.value);
+  }
+
+  console.log(value);
+
+  return (
+    <EditorProvider>
+      <Editor
+        value={value}
+        onChange={onChange}
+        className="h-[230px] dark:text-white "
+      >
+        <Toolbar>
+          <BtnBold />
+          <BtnItalic />
+          <BtnUnderline />
+          <BtnStrikeThrough />
+          <Separator />
+          <BtnNumberedList />
+          <BtnBulletList />
+          <Separator />
+          <BtnLink />
+        </Toolbar>
+      </Editor>
+    </EditorProvider>
+  );
+}
