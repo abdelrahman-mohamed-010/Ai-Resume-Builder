bb14f9b12ea6c9bcf5b959285a1b1119eeb6d47c
body <abdo.mhamed122yahoo.com@gmail.com>
2024-11-19 14:05:17 +0200

    update: border color of toggle menu

--- frontend/src/components/Sidebar/ToggleClose.tsx
+++ frontend/src/components/Sidebar/ToggleClose.tsx
@@ -14,7 +14,7 @@ const ToggleClose: React.FC<ToggleCloseProps> = ({ open, setOpen }) => {
       className={`absolute bottom-0 left-0 right-0 max-sm:z-50 max-sm:fixed  max-sm:bg-slate-300  border-t  border-slate-300 transition-colors md:hover:bg-slate-100 dark:md:hover:bg-gray-700 dark:text-white ${
         open
           ? " max-sm:bg-white max-sm:dark:bg-dark-background max-sm:w-[225px] border-r border-slate-300"
-          : "max-sm:max-w-14 max-sm:max-h-14 max-sm:bg-gray-100 max-sm:dark:bg-slate-950 max-sm:border-2 max-sm:border-r-2 max-sm:dark:border-2 max-sm:rounded-full max-sm:bottom-3 max-sm:left-3"
+          : "max-sm:max-w-14 max-sm:max-h-14 max-sm:bg-gray-100 max-sm:dark:bg-slate-950 max-sm:border-2 max-sm:border-r-2 max-sm:border-slate-400 max-sm:dark:border-2 max-sm:rounded-full max-sm:bottom-3 max-sm:left-3"
       }`}
     >
       <div className="flex items-center p-2 max-sm:justify-center">
